b703d09351f45beff37f3d88435cee9c3213fe47
Leon Byford <ldjb20@bath.ac.uk>
2014-04-16 13:34:39 +0100

    create users page

---
+++ siteroot/user.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+require_once('core.php');
+$acc = new Account;
+$user = $acc->user_from_id(1);
+$smarty->assign('user', $user);
+$smarty->assign('name', $_SESSION['user']['displayname']);
+
+$scheduler = new Scheduler;
+$t = $scheduler->tournament_from_id(1);
+
+
+//$smarty->assign('content', join_templates($latest_tournaments, $latest_fixtures, $latest_results));
+$smarty->display('template/users.html');
+?>

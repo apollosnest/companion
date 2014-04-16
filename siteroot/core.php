a0ce75457cc6f4c37350eb6682f9347d23320ffe
Leon Byford <ldjb20@bath.ac.uk>
2014-04-16 13:31:14 +0100

    turn off Smarty debugging

--- siteroot/core.php
+++ siteroot/core.php
@@ -21,7 +21,7 @@ function join_templates()
 /** Instance of template manager **/
 $smarty = new Smarty;
 //$smarty->force_compile = true;
-$smarty->debugging = true;
+$smarty->debugging = false;
 $smarty->caching = false;
 $smarty->cache_lifetime = 120;
 
